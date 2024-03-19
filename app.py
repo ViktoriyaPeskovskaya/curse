7d1b9fe0dc921357c11efc2202b992810ea505af
ViktoriyaPeskovskaya <vikap160903@gmail.com>
2024-03-19 23:56:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-
+@app.route('/')
+def bye_world():
+    return 'Bye World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
